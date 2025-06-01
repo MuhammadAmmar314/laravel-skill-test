f78a12c71d2f78105073aa19c4b44b39f9cfeac3
Muhammad Ammar Abdurrahim <muhammadammar314@gmail.com>
2025-06-01 08:31:33 +0700

    update relational database for Post and User

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,6 +10,14 @@ class Post extends Model
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
 
+    protected $fillable = [
+        'title',
+        'content',
+        'is_draft',
+        'published_at',
+        'user_id',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
